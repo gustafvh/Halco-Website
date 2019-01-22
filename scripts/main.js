2b028524f78d3e9e918eb65ac368c81ecd611b91
Gustaf <gustaf.halvardsson@gmail.com>
2019-01-22 18:54:57 +0100

    fixed main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,3 @@
-/*
-
-<!-- How to Write a Function -->
-function functionName() {
-	// body of the function
-}
-<!-- How to Change CSS of an ID on the Page -->
-document.getElementById("idName").style.property = 'value';
-<!-- How to Run a Function on Button Click -->
-< button onclick="functionName()">buttonName</button>
-<!-- Create a Variable -->
-var nameOfVariable = value;
-<!-- How to Use an If-Statement -->
-if (condition) {
-	// do this if the condition is true
-}
-<!-- How to Set Text of an HTML Element in JS -->
-document.getElementById("idName").innerHTML = 'newTextInElt'
-
-// */
-// function buttonHoverFunction(){
-//   document.getElementById("btn-primary").style.margin = '100';
-// }
-
-
-
-// function buttonClickFunction() {
-//   document.getElementById("btn-primary").style.margin = '90';
-
-
-//}
 
 
 
